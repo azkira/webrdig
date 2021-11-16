8327151c7c3612dc48d6928cf2309f8db4ed3107
azkira <azkirasyid@gmail.com>
2021-11-16 19:30:53 +0700

    jadwal hardcode

---
+++ src/components/Tables/modul1Table.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Nama
+          </th>
+          <th class="text-left">
+            NRP
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in students"
+          :key="item.name"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.nrp }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  data () {
+      return {
+        students: [
+          {
+            name: 'Muhammad Azki',
+            nrp: 11212121259,
+          },
+          {
+            name: 'Adima',
+            nrp: 121121212122,
+          },
+        ],
+      }
+    },
+}
+</script>
---
+++ src/views/Teams.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Modul 1</h1>
+    <Modul1Table/>
+    <h1>Modul 2</h1>
+    <h1>Modul 3</h1>
+    <h1>Modul 4</h1>
+    <h1>Modul 5</h1>
+  </div>
+</template>
+
+<script>
+import Modul1Table from '../components/Tables/modul1Table.vue'
+export default {
+  name: 'Teams',
+  components: {
+    Modul1Table
+  }
+}
+</script>
