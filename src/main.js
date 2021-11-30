b48bf77335fbb7ef9ee513f78c2bc82e438a1121
Adima Putra <adima.mahardika117@gmail.com>
2021-11-30 18:20:02 +0700

    menambah method pada button di AND

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import VueMqtt from 'vue-mqtt'
+
 
 Vue.config.productionTip = false
+
+const mqttoptions = {
+  username: '',
+  password: ''
+}
+
+Vue.use(VueMqtt, 'ws://13.76.193.191:9001', mqttoptions);
 
 new Vue({
   router,
--- src/views/modules/percobaan/dasarRdig/And.vue
+++ src/views/modules/percobaan/dasarRdig/And.vue
@@ -5,8 +5,8 @@
       <v-col class="grid">
         <v-row>
           <v-col>
-            <v-btn @click="switch1('0')" color="primary" elevation="2" icon>0</v-btn>
-            <v-btn @click="switch1('1')" color="primary" elevation="2" icon>1</v-btn>
+            <v-btn @click="switch1('0'); ledOff1()" color="primary" elevation="2" icon>0</v-btn>
+            <v-btn @click="switch1('1'); ledOn1()" color="primary" elevation="2" icon>1</v-btn>
             <v-col>
               <v-btn color="primary" elevation="2" icon>{{input1}}</v-btn>
             </v-col>
@@ -14,8 +14,8 @@
         </v-row>
         <v-row>
           <v-col>
-            <v-btn @click="switch2('0')" color="primary" elevation="2" icon>0</v-btn>
-            <v-btn @click="switch2('1')" color="primary" elevation="2" icon>1</v-btn>
+            <v-btn @click="switch2('0'); ledOff2()" color="primary" elevation="2" icon>0</v-btn>
+            <v-btn @click="switch2('1'); ledOn2()" color="primary" elevation="2" icon>1</v-btn>
             <v-col>
               <v-btn color="primary" elevation="2" icon>{{input2}}</v-btn>
             </v-col>
@@ -61,61 +61,95 @@
 <script>
 //import Socket from '../../../../socket';
 
-export default {
-  name: "And",
-  data() {
-    return {
-      input1: '0',
-      input2: '0',
-      temp1: '0',
-      temp2: '0',
-      output: '',
-      state: false
-    };
-  },
-/*  mounted() {
-    // Connected 
-    Socket.$on("connected", () => {
-      this.ws.connected = true;
-    })
-    // Disconnected
-    Socket.$on("message", (json) => {
-      this.ws.connected = false;
-    })
-  },*/
-  methods: {
-    switch1(number) {
-      if (this.state) {
-        this.input1 = '0';
-        this.state = false;
-      }
-      this.input1 = `${number}`;
-      this.temp1 = `${number}`
+  export default {
+    name: "And",
+    data() {
+      return {
+        input1: '0',
+        input2: '0',
+        temp1: '0',
+        temp2: '0',
+        output: '',
+        state: false
+      };
     },
-    switch2(number) {
-      if (this.state) {
-        this.input2 = '0';
-        this.state = false;
-      }
-      this.input2 = `${number}`;
-      this.temp2 = `${number}`
-    },
-    get() {
-      if (this.temp1 === '0' && this.temp2 === '0'){
-        this.output = '0';
-      }
-      if (this.temp1 === '0' && this.temp2 === '1'){
-        this.output = '0';
-      }
-      if (this.temp1 === '1' && this.temp2 === '0'){
-        this.output = '0';
-      }
-      if (this.temp1 === '1' && this.temp2 === '1'){
-        this.output = '1';
-      }
+  /*  mounted() {
+      // Connected 
+      Socket.$on("connected", () => {
+        this.ws.connected = true;
+      })
+      // Disconnected
+      Socket.$on("message", (json) => {
+        this.ws.connected = false;
+      })
+    },*/
+    methods: {
+      switch1(number) {
+        if (this.state) {
+          this.input1 = '0';
+          this.state = false;
+        }
+        this.input1 = `${number}`;
+        this.temp1 = `${number}`
+      },
+      switch2(number) {
+        if (this.state) {
+          this.input2 = '0';
+          this.state = false;
+        }
+        this.input2 = `${number}`;
+        this.temp2 = `${number}`
+      },
+      get() {
+        if (this.temp1 === '0' && this.temp2 === '0'){
+          this.output = '0';
+        }
+        if (this.temp1 === '0' && this.temp2 === '1'){
+          this.output = '0';
+        }
+        if (this.temp1 === '1' && this.temp2 === '0'){
+          this.output = '0';
+        }
+        if (this.temp1 === '1' && this.temp2 === '1'){
+          this.output = '1';
+        }
+      },
+      ledOn1(){
+        this.$mqtt.publish('esp8266/led_suhu', 'led1-active', (err) => {
+        if(!err){
+          this.state = "Berhasil Menyalakan lampu";
+          //alert("Berhasil Memberi Minum");
+            }
+          })
+        },
+      ledOff1(){
+        this.$mqtt.publish('esp8266/led_suhu', 'led1-deactive', (err) => {
+        if(!err){
+          this.state = "Berhasil Mematikan lampu";
+          //alert("Berhasil Memberi Minum");
+            }
+          })
+      },
+      ledOn2(){
+        this.$mqtt.publish('esp8266/led_suhu', 'led2-active', (err) => {
+        if(!err){
+          this.state = "Berhasil Menyalakan lampu";
+          //alert("Berhasil Memberi Minum");
+            }
+          })
+        },
+      ledOff2(){
+        this.$mqtt.publish('esp8266/led_suhu', 'led2-deactive', (err) => {
+        if(!err){
+          this.state = "Berhasil Mematikan lampu";
+          //alert("Berhasil Memberi Minum");
+            }
+          })
+      }  
     }
+    
+
   }
-};
 </script>
 
 <style scoped>
